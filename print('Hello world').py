c66ffac352b584608cb35eba7c14116baca4ad52
F0reVxR <f1n2l0v3_o6e9@tutamail.com>
2025-09-19 13:17:01 +0300

    321

--- print('Hello world').py
+++ print('Hello world').py
@@ -1,9 +1,24 @@
 import random
+import math
 
-print('Hello world')
-
+"""
 a = random.randint(10, 10000)
 print(a)
+"""
+#First task
+Length = int(17)
+Width = float(19)
+Square = Length * Width
+print('Площадь участка', Square)
 
+#Second task
+Pi = math.pi
+r = int(input('Введите радиус '))
+Sq = Pi * (r^2)
+Circ_Length = 2 * Pi * r
+print('Площадь - ', Sq, 'Длина окружности - ', Circ_Length)
 
-
+#Third task
+a = float(input('Введите старший коэффициент '))
+b = float(input('Введите средний коэффициент '))
+c = float(input('Введите свободный коэффициент '))
