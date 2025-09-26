981724153f1257479f89aa62302a4c9390f6c827
F0reVxR <f1n2l0v3_o6e9@tutamail.com>
2025-09-26 15:35:08 +0300

    practice

---
+++ pract.py
@@ -0,0 +1,50 @@
+import math
+"""
+a = input()
+b = input()
+print(float(int(a) // int(b))) #/ выдаст float, а // целочисленное деление
+"""
+
+"""
+#Комплексные 
+z1 = 1 + 3j
+z2 = 8 - 9j
+z3 = input()
+z4 = input()
+print(complex(z1 * z2))
+
+a = float(input())
+print(a ** 0.5)#sqrt
+"""
+
+"""
+a = float(input())
+b = float(input())
+c = float( float(a ** 2) + float(b ** 2) ) ** 0.5
+
+sina = (a / c)
+sinb = (b / c) 
+
+if(sina > sinb):
+  print(sina)
+else:
+  print(sinb)
+print(c)
+"""
+
+"""
+a = 1000
+while (a > 0):
+    print(a)
+    a = a - 7
+"""
+
+st = 'Skebob'
+print(st[0])
+
+for c in st:
+    print(c * 30)
+
+a = {1:'444', 2:'888'}
+print(str(a))
+
