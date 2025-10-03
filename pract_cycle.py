48ac1856eff7d085c1f236cb4ea68a359e309d12
F0reVxR <f1n2l0v3_o6e9@tutamail.com>
2025-10-03 13:19:07 +0300

    334

--- pract_cycle.py
+++ pract_cycle.py
@@ -7,23 +7,20 @@ match(int(input('Введите операцию (0 / 1 / 2 / 3) - '))):
     case 0:
         for i in range(0,len(Numb)):
             Numb[i] = 0
-        print(Numb)
     case 1:
         for i in range(0,len(Numb)):
             Numb[i] = Numb[i] * 2
-        print(Numb)
     case 2:
         for i in range(0,len(Numb),2):
             Numb[i] = Numb[i] + 10
-        print(Numb)
     case 3:
         for i in range(0,len(Numb),3):
             if Numb[i] >= 0:
                 Numb[i] = float(Numb[i] ** 0.5)
             else:
                 Numb[i] = 0
-        print(Numb)
     case _:
         for i in range(0,len(Numb)):
-            Sum += Numb[i]
+            Sum += Numb[i] #Либо Sum = sum(Numb)
         print('Сумма числецов в твой списка: ', Sum)
+print(Numb)
