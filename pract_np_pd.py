7df9f1c6ce534c7989066b28bf2f893841c0e4fa
F0reVxR <f1n2l0v3_o6e9@tutamail.com>
2025-10-15 11:50:37 +0300

    131231231

--- pract_np_pd.py
+++ pract_np_pd.py
@@ -3,13 +3,15 @@ import pandas as pd
 import random as rd
 
 r = {}
-for i in range(1,10):
+for i in range(1,3):
     NA = input('Введите имя: ')
     FN = input('Введите фамилию: ')
     PN = input('Введите отчество: ')
     DT = input('Введите дату рождения: ')
     id = rd.randint(100, 999)
-    n = dict[ 'Имя': NA, 'Фамилия': FN, 'Отчество': PN, 'Дата рождения': DT]
-    r.update({id:n})
-    
-print(r)
+    n = { 'Имя': NA, 'Фамилия': FN, 'Отчество': PN, 'Дата рождения': DT}
+    r.update({id:n})  
+#print(r)
+
+df = pd.DataFrame(r.values(), index=r.keys())
+print(df)
